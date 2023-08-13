04ef9037206680d21aca3c7e3dd35c9743a0c813
Lotachi <lotaoko2@gmail.com>
2023-08-13 23:45:08 +0100

    Semi final additions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import analytics from '@vercel/analytics';
+
+analytics();
 
 ReactDOM.render(
   <React.StrictMode>
+    
     <App />
   </React.StrictMode>,
   document.getElementById('root')
