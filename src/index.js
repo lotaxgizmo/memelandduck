5a933a4e03d661271bf7d4690c8b2ff0345d1eea
Lotachi <lotaoko2@gmail.com>
2023-08-14 18:03:50 +0100

    jfjf

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
-import analytics from '@vercel/analytics';
 
-analytics();
 
 ReactDOM.render(
   <React.StrictMode>
